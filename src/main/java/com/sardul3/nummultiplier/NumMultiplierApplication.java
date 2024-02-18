e79382539791ffa179bc0c5578f3b63f6e7744d2
Sagar <poudelsagar530@gmail.com>
2024-02-18 17:38:25 -0600

    Initial project setup
    revert

---
+++ src/main/java/com/sardul3/nummultiplier/NumMultiplierApplication.java
@@ -0,0 +1,13 @@
+package com.sardul3.nummultiplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NumMultiplierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NumMultiplierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sardul3/nummultiplier/NumMultiplierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sardul3.nummultiplier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NumMultiplierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
